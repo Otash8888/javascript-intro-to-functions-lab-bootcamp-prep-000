b3274516ea71b88bdcc94f1ebc9a32c773bbf185
Otabek Kamalov <otakam86@gmail.com>
2018-06-08 00:26:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if (string.toLowerCase()===string){
+    return "I can't hear you!"
+  }
+  else if (string.toUpperCase()===string){
     return 
+    
   }
   
 }
